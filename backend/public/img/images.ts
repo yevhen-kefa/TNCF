36e4e3636d5954a7e164eed5577b3fc2512ee000
humariu506 <rayan.essaidi2006@gmail.com>
2026-03-24 13:42:20 +0100

    Added images and stylesheet for php alternative

---
+++ backend/public/img/images.ts
@@ -0,0 +1,45 @@
+import alertSvg  from './alert.svg';
+import arriveSvg  from './arrive.svg';
+import arrowLeftSvg  from './arrow_l.svg';
+import arrowRightSvg  from './arrow_r.svg';
+import boxGoldSvg  from './box_g.svg';
+import boxSvg    from './box.svg';
+import cartSvg   from './cart.svg';
+import clockSvg  from './clock.svg';
+import departSvg  from './depart.svg';
+import layerSvg  from './layer.svg';
+import logoSvg   from './logo.svg';
+import mailGSvg  from './mail_gold.svg';
+import mailSvg   from './mail.svg';
+import passSvg   from './pass.svg';
+import persoSvg  from './person.svg';
+import persoWhiteSvg  from './person_white.svg';
+import searcSvg  from './search.svg';
+import shielSvg  from './shield.svg';
+import switcSvg  from './switch.svg';
+import tickeSvg  from './ticket.svg';
+
+
+export{
+    alertSvg,
+    arriveSvg,
+    arrowLeftSvg,
+    arrowRightSvg,
+    boxGoldSvg,
+    boxSvg,
+    cartSvg,
+    clockSvg,
+    departSvg,
+    layerSvg,
+    logoSvg,
+    mailGSvg,
+    mailSvg,
+    passSvg,
+    persoSvg,
+    persoWhiteSvg,
+    searcSvg,
+    shielSvg,
+    switcSvg,
+    tickeSvg
+
+}
