5b5dc55d279d336f41a0c151a7a24b6ba3a3110c
yevhen-kefa <yevhenkefa@gmail.com>
2026-06-08 18:47:04 +0200

    Corection erreur

--- db/init.sh
+++ db/init.sh
@@ -4,4 +4,6 @@ mongoimport --db tncf --collection utilisateur --type csv --headerline --file /d
 mongoimport --db tncf --collection train --type csv --headerline --file /docker-entrypoint-initdb.d/config.train.csv
 mongoimport --db tncf --collection voyage --type csv --headerline --file /docker-entrypoint-initdb.d/config.voyage.csv
 mongoimport --db tncf --collection billet --type csv --headerline --file /docker-entrypoint-initdb.d/config.billet.csv
+
+echo "=========== Finish ==========="
 echo "=========== Import completed ==========="
